8bce0a9a4542cf09ae276281ad3a80953aa843a1
Victor Rodrigues dos Santos <vrsisa@hotmail.com>
2022-02-17 00:12:04 -0300

    Upload de Imagem

--- Blog/ViewModels/Accounts/RegisterViewModel.cs
+++ Blog/ViewModels/Accounts/RegisterViewModel.cs
@@ -1,14 +1,15 @@
 using System.ComponentModel.DataAnnotations;
-
-namespace Blog.ViewModels
+namespace Blog.ViewModels.Accounts;
+public class RegisterViewModel
 {
-    public class RegisterViewModel.Accounts
-    {
-        [Required(ErrorMessage = "O nome é obrigatório")]
+
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
     public string Name { get; set; }
 
     [Required(ErrorMessage = "O E-mail é obrigatório")]
     [EmailAddress(ErrorMessage = "O E-mail é invalido")]
     public string Email { get; set; }
+
 }
-}
+
